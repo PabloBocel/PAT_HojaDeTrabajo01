425801eab15b714c007a01d72d7f4d30dc9e3b50
Pablo <andresbocel0@gmail.com>
2023-09-07 12:13:49 -0600

    Ejercicio3

--- HojaDeTrabajo01/Ejercicio03.cpp
+++ HojaDeTrabajo01/Ejercicio03.cpp
@@ -3,13 +3,16 @@
 
 bool Ejercicio03::isPowerOfTwo(int n)
 {
+        if (n == -1) {
+            return true;
+        }
         if (n == 0) {
             return false;
         }
         if (n == 1) {
             return true;
         }
-        if (n % 2 != 0) {
+        if ((n % 2) != 0) {
             return false;
         }
         return isPowerOfTwo(n / 2);
